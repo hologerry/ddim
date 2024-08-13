a87f6782ed3991566bd6e2a42432e5335ed6c43a
Yue Gao <hologerry@gmail.com>
2024-08-14 01:48:08 +0800

    update

--- train.sh
+++ train.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python main.py --data_root /data/Dynamics/lsun_datasets --config church.yml --exp /data/Dynamics/ddim_log --doc church --ni
+python main.py --data_root /data/Dynamics/lsun_data --config church.yml --exp /data/Dynamics/ddim_log --doc church --ni
